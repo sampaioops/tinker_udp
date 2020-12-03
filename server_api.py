1af7cb70c2cd5cbebbc48e1d2767df05086074c4
Daniel <sampaioops@gmail.com>
2020-12-03 01:35:47 -0300

    Bugfixs

--- server_api.py
+++ server_api.py
@@ -39,7 +39,7 @@ while(True):
         data_e_hora_atuais = datetime.now()
         data_e_hora_em_texto = data_e_hora_atuais.strftime("%d/%m/%Y %H:%M")
         UDPServerSocket.sendto(str.encode('SENSOR ATIVADO! - ' + data_e_hora_em_texto), serverAddressPort)
-    else:
+    #else:
         #UDPServerSocket.sendto(str.encode(''), serverAddressPort)
 
 
