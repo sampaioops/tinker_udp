c8cea589acbe2d0df52478ea5cc69b56bddb50dc
Daniel <sampaioops@gmail.com>
2020-12-05 03:09:36 -0300

    ..

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,6 +2,7 @@ import os
 import telepot
 import urllib
 import json
+import time
 import ASUS.GPIO as GPIO
 
 pir = 26
@@ -13,17 +14,19 @@ GPIO.setwarnings(False)
 GPIO.setup(pir, GPIO.IN)
 
 
-bot = telepot.Bot("1419698093:AAHZJjuVqY9lzZ07sz9CVZnTZm-m4wgZSII")
 
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
-
     if command == 'On':
         while True:
-
             if GPIO.input(pir) == GPIO.HIGH:
                 bot.sendMessage(chat_id, "Sensor ativado!")
+    
 
 
-bot.message_loop(handle)                
+bot = telepot.Bot('1419698093:AAHZJjuVqY9lzZ07sz9CVZnTZm-m4wgZSII')
+bot.message_loop(handle)            
+
+while 1:
+    time.sleep(10)
