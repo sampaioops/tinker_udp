083b1495b7c8e6d5e5b6fe7cc1ee359cfd506912
Daniel <sampaioops@gmail.com>
2020-12-05 12:06:56 -0300

    ..

--- telegram_bot.py
+++ telegram_bot.py
@@ -16,6 +16,7 @@ GPIO.setup(pir, GPIO.IN)
 
 
 def handle(msg):
+    print('teste')
     chat_id = msg['chat']['id']
     command = msg['text']
     if command == 'Ativa Sensor':
@@ -26,11 +27,6 @@ def handle(msg):
             
             if GPIO.input(pir) == GPIO.HIGH:
                 bot.sendMessage(chat_id, "Sensor ativado!")
-
-    
-            
-
-    
 
 
 bot = telepot.Bot('1419698093:AAHZJjuVqY9lzZ07sz9CVZnTZm-m4wgZSII')
