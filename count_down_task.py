2dd944ec5e119b2805e36cc2273eeb83248f1dff
Daniel <sampaioops@gmail.com>
2020-12-05 12:42:56 -0300

    testando..

--- count_down_task.py
+++ count_down_task.py
@@ -1,16 +1,10 @@
-import time
-import os
-import telepot
-import urllib
-import json
-import time
 import ASUS.GPIO as GPIO
 
-
 class CountdownTask: 
-
+      
     def __init__(self): 
         self._running = True
+      
 
 pir = 26
 
@@ -18,16 +12,11 @@ GPIO.setmode (GPIO.BOARD)
 GPIO.setwarnings(False)
 
 GPIO.setup(pir, GPIO.IN)
-      
+    
 def terminate(self): 
     self._running = False
       
-def run(self): 
+def run(self, chat_id, bot): 
     while self._running: 
-        while True:
-            
-            if command == 'break':
-                break
-            
-            if GPIO.input(pir) == GPIO.HIGH:
-                bot.sendMessage(chat_id, "Sensor ativado!")
+        if GPIO.input(pir) == GPIO.HIGH:
+            bot.sendMessage(chat_id, "Sensor ativado!")
--- telegram_bot.py
+++ telegram_bot.py
@@ -6,36 +6,41 @@ import time
 import ASUS.GPIO as GPIO
 from threading import Thread 
 
+class CountdownTask: 
+      
+    def __init__(self): 
+        self._running = True
+      
+def terminate(self): 
+    self._running = False
+      
+def run(self, chat_id): 
+    while self._running: 
+        if GPIO.input(pir) == GPIO.HIGH:
+            bot.sendMessage(chat_id, "Sensor ativado!")
+
 pir = 26
 
- 
 GPIO.setmode (GPIO.BOARD)
 GPIO.setwarnings(False)
 
 GPIO.setup(pir, GPIO.IN)
 
-iniciaSensor = False
-
-def executaSensor(chat_id):
-    while iniciaSensor:
-        if GPIO.input(pir) == GPIO.HIGH:
-            bot.sendMessage(chat_id, "Sensor ativado!")
+c = CountdownTask()
 
 def handle(msg):
-    
     chat_id = msg['chat']['id']
     command = msg['text']
     if command == 'start sensor':
-        iniciaSensor = True
-        t = Thread(target = executaSensor, args =(chat_id, )) 
-        t.start()
+        t = Thread(target = c.run, args =(chat_id, )) 
+        t.start() 
     elif command == 'stop sensor':
-        iniciaSensor = False
+        c.terminate()
             
-
 
 bot = telepot.Bot('1419698093:AAHZJjuVqY9lzZ07sz9CVZnTZm-m4wgZSII')
 bot.message_loop(handle)            
 
 while 1:
     time.sleep(10)
+
